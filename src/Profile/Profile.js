e4bd46c0c1497be3a9e15d02d3c49fc4632b707c
eagobert <eagobert@hotmail.com>
2018-06-14 22:05:26 -0500

    profile image converting to blob format

--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react'
 import ProfilePhoto from '../Profile/ProfilePhoto'
 // import Locations from './Locations'
 //import Interests from './Interests'
+import '../Profile/Profile.css'
 
 class Profile extends Component {
     render(){
 
         return(
         <div className="profile">
+        <h3>Profile</h3>
         <ProfilePhoto />
         {/* <Locations />
         <Interests /> */}
--- src/Profile/ProfilePhoto.js
+++ src/Profile/ProfilePhoto.js
@@ -1,9 +1,40 @@
 import React, { Component } from 'react'
 
 class ProfilePhoto extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            file: null
+        }
+    }
+
+    handleImageUpload = function(e){
+        const selectedFile = e.target.files[0]
+        //need to convert to acceptable format for posting...
+        const imgData = window.URL.createObjectURL(selectedFile)
+
+        this.setState({
+            file: imgData
+        })
+        //console.log(this.state)
+    }.bind(this)
+
+    handleImageSave = function(e){
+        e.preventDefault();  
+    }
+
     render(){
         return(
-            <div></div>
+            <div className="profile-photo">
+                <div className="img-preview">
+
+                </div>
+                <form  id="myPhotoForm" name="myPhotoForm" onSubmit={this.handleImageSave}>
+                    <input type="file" id="imgUrl" name="imgUrl" onChange={this.handleImageUpload}/>
+                    <button type="submit" value="Submit!">Upload</button>
+                </form>
+            </div>
         )
     }
 }
