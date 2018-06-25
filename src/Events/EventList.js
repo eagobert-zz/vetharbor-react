547d60703a5b07d9aac6337efffdbc92d7d5642c
eagobert <eagobert@hotmail.com>
2018-06-25 13:04:26 -0500

    still trying to update eventlist

--- src/Events/EventList.js
+++ src/Events/EventList.js
@@ -3,7 +3,7 @@ import '../Events/Events.css'
 
 class EventList extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             userEvents: []
@@ -16,59 +16,62 @@ class EventList extends Component {
 
         //Get active user's events from vetharbor.json
         fetch(`http://localhost:8088/events?userId=${ActiveUser.id}`)
-        
-        //convert get to json data
-        .then(r => r.json())
 
-        .then(allEvents => {
-            let events = []
-            //for each event tied to active user's id, loop thru and ...
-            allEvents.forEach(event => {
-                
-                //make a fetch request to eventbrite with the event's id
-                fetch(`https://www.eventbriteapi.com/v3/events/${event.id}/?token=DSOBPDFILDDO4LF47MRJ`)
+            //convert get to json data
+            .then(r => r.json())
 
-                //convert response to json
-                .then(r => r.json())
+            .then(allEvents => {
+                let events = []
+                //for each event tied to active user's id, loop thru and ...
+                allEvents.forEach(event => {
 
-                //return the data???
-                .then(ue => {
-                    events.push(ue)
-                    this.setState({
-                        userEvents: events
-                    })
+                    //make a fetch request to eventbrite with the event's id
+                    fetch(`https://www.eventbriteapi.com/v3/events/${event.id}/?token=DSOBPDFILDDO4LF47MRJ`)
+
+                        //convert response to json
+                        .then(r => r.json())
+
+                        //return the data???
+                        .then(ue => {
+                            events.push(ue)
+                            // this.userEvents = events
+                            this.setState({
+                                userEvents: events
+                            })
+                        })
                 })
             })
-            
-        })
+    }
+
+    
+    shouldComponentUpdate(nextProps, nextState) {
+        if (this.state.userEvents !== nextState.userEvents) {
+            return true
+        }
 
     }
 
-    componentDidUpdate(nextState){
-        if(this.state.userEvents !== nextState.userEvents){
-            return true;
-        }
+    componentDidUpdate() {
+     this.loadEvents()
     }
 
     //run the function in a component did mount
-    componentDidMount(){
+    componentDidMount() {
         console.log("component did mount")
         this.loadEvents()
     }
 
     render() {
 
-        console.log("render called", this.state.userEvents)
-        
         return (
             <div className="event-list">
-            <h3>These are events</h3>
-            {this.state.userEvents.map(ues => {
-               return <h1 key={ues.id}>{ues.name.text}</h1>
-            })}
-            
+                <h3>These are events</h3>
+                {this.state.userEvents.map(ues => {
+                    return <h1 key={ues.id}>{ues.name.text}</h1>
+                })}
+
             </div>
-            );
+        );
 
     }
 }
