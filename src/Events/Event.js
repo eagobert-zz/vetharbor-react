a491871e4b62ff8960208b9dd2e887db5af39332
eagobert <eagobert@hotmail.com>
2018-06-28 14:42:35 -0500

    onload events now displaying images

--- src/Events/Event.js
+++ src/Events/Event.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 
 class Event extends Component {
     render(){
+      
         return(
             <span>
-                {/* <div className="event-img">
-                    <img src={this.props.event.logo.original.url} alt="" />
-                </div> */}
+                <div className="event-img">
+                    {this.props.event.logo === null && <img src="http://placehold.it/175x175" alt="" />}
+
+                    {this.props.event.logo !== null && <img src={this.props.event.logo.original.url} alt="" />}
+                </div>
                 <h4>{this.props.event.name.text}</h4>
                 <p>{this.props.event.description.text}</p>
             </span>
